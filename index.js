39055c9d301d7c5f560efaaaf147f8ac2a3a1dda
ray.tsang <ray.tsang@codpayment.com>
2019-08-26 12:46:45 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Parser } = require('json2csv');
+const fs = require('fs');
+
+const fields = ['car', 'price', 'color'];
+const myCars = [
+    {
+        "car": "Audi",
+        "price": 40000,
+        "color": "blue"
+    }, {
+        "car": "BMW",
+        "price": 35000,
+        "color": "black"
+    }, {
+        "car": "Porsche",
+        "price": 60000,
+        "color": "green"
+    }
+];
+
+const json2csvParser = new Parser({ fields });
+const csv = json2csvParser.parse(myCars);
+
+console.log(csv);
+
+fs.writeFile('file.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+});
